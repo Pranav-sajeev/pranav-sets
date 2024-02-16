f2b392d607d1963decbaf68ab91604baf1f02795
Pranav-sajeev <pranavs.bsc23@rvu.edu.in>
2024-02-16 14:43:56 +0530

    test

--- set01/problem02.c
+++ set01/problem02.c
@@ -4,9 +4,9 @@
 int main()
 {
     int a,b,sum;
-    printf("Enter the value of a\n");
+    printf("Enter the value of a:\n");
     scanf("%d",&a);
-    printf("Enter the value of b\n");
+    printf("Enter the value of b:\n");
     scanf("%d",&b);
     sum=a+b;
     printf("The sum of %d and %d is %d",a,b,sum);
